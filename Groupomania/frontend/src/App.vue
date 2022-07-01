29284ea09bd063d226215cd1e9033ce0ef4e6bda
LorisBST <bouissetloris@gmail.com>
2022-07-01 20:47:48 +0200

    routes vues

--- Groupomania/frontend/src/App.vue
+++ Groupomania/frontend/src/App.vue
@@ -1,42 +1,16 @@
 <script>
-import LoginVue from "./components/Login.vue"
+import { RouterView } from 'vue-router';
 export default {
-  name: "App",
-  components: {
-    LoginVue
+  components:{
+    RouterView
   }
 }
 </script>
 
 <template>
-  <LoginVue />
+
+<router-view></router-view>
 </template>
 
 <style>
-
-body { font-weight: bold;
-  font-family: "lato",serif!important;}
-
-#tab-register, #tab-login, #btnRegister, #btnLogin { font-weight: bold;
-  font-family: "lato", serif!important;}
-
-
-.nav-pills .pill-1 .nav-link:not(.active) {
-  background-color: darkgrey!important;
-  color: black;
-}
-
-.nav-pills .pill-2 .nav-link:not(.active) {
-  background-color: darkgrey!important;
-}
-
-.nav-pills .pill-1 .nav-link {
-  background-color: #FD2D01!important;
-  color: white;
-}
-
-.nav-pills .pill-2 .nav-link {
-  background-color: #FD2D01!important;
-  color: white;
-}
 </style>
--- Groupomania/frontend/src/main.js
+++ Groupomania/frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import router from "./router"
 import BootstrapVue3 from 'bootstrap-vue-3'
 import App from './App.vue'
 
@@ -7,6 +8,6 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 
 const app = createApp(App)
-
 app.use(BootstrapVue3)
+app.use(router)
 app.mount('#app')
